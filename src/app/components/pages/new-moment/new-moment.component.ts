635fce293ab1b45a69372094728786f41e162b98
Mateus Scarin <mateus.scarin@gmail.com>
2022-10-17 14:44:55 -0300

    feat: Finalizando sistema de mensagens

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -2,6 +2,8 @@ import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Moment } from 'src/app/Moment';
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-moment',
@@ -11,17 +13,17 @@ import { MomentService } from 'src/app/services/moment.service';
 export class NewMomentComponent implements OnInit {
   btnText = 'Compartilhar!';
 
-  constructor(private momentService: MomentService) {}
+  constructor(
+    private momentService: MomentService,
+    private messagesService: MessagesService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
   async createHandler(moment: Moment) {
-    /* const formData = new FormData();
-    formData.append('title', moment.title);
-    formData.append('description', moment.description);
-    if (moment.image) {
-      formData.append('image', moment.image);
-    } */
     await this.momentService.createMoment(moment).subscribe();
+    this.messagesService.add('Momento adicionado com sucesso!');
+    this.router.navigate(['/']);
   }
 }
