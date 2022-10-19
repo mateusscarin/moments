0128702d3cf7e6d7116a2f44813841ad5500f197
Mateus Scarin <mateus.scarin@gmail.com>
2022-10-19 08:12:25 -0300

    feat: inserindo comentários

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -4,6 +4,14 @@ import { MomentService } from 'src/app/services/moment.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
 import { MessagesService } from 'src/app/services/messages.service';
+import {
+  FormGroup,
+  FormGroupDirective,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import { CommentService } from 'src/app/services/comment.service';
+import { Comment } from 'src/app/Comment';
 
 @Component({
   selector: 'app-moment',
@@ -14,10 +22,12 @@ export class MomentComponent implements OnInit {
   moment?: Moment;
   faTimes = faTimes;
   faEdit = faEdit;
+  commentForm!: FormGroup;
 
   constructor(
     private momentService: MomentService,
     private messagesService: MessagesService,
+    private commentService: CommentService,
     private route: ActivatedRoute,
     private router: Router
   ) {}
@@ -26,12 +36,39 @@ export class MomentComponent implements OnInit {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.momentService
       .getMoment(id)
-      .subscribe((response) => (this.moment = response.body));
+      .subscribe((response) => (this.moment = response.body!));
+    this.commentForm = new FormGroup({
+      text: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required]),
+    });
   }
 
   async removeHandler(id: number) {
     await this.momentService.removeMoment(id).subscribe();
     this.messagesService.add('Momento excluído com sucesso!');
     this.router.navigate(['/']);
+  }
+
+  async onSubmit(formDirective: FormGroupDirective) {
+    if (this.commentForm.invalid) {
+      return;
+    }
+    const data: Comment = this.commentForm.value;
+    data.moment = {} as Moment;
+    data.moment!.id = this.moment!.id;
+    await this.commentService.createComment(data).subscribe((response) => {
+      this.moment!.comments!.push(response.body!);
+      this.messagesService.add('Comentário adicionado com sucesso!');
+    });
+    this.commentForm.reset();
+    formDirective.resetForm();
+  }
+
+  get text() {
+    return this.commentForm.get('text')!;
+  }
+
+  get username() {
+    return this.commentForm.get('username')!;
   }
 }
--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { Comment } from 'src/app/Comment';
+import { Response } from 'src/app/Response';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommentService {
+  private baseApiUrl = environment.baseApiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  createComment(comment: Comment): Observable<Response<Comment>> {
+    return this.http.post<Response<Comment>>(
+      `${this.baseApiUrl}/comment`,
+      comment
+    );
+  }
 }
